56f7c71d806e5ba3a778b190c7ec3c60fe1b40ca
kashyap-5 <89954613+kashyap-5@users.noreply.github.com>
2022-12-04 15:08:01 +0530

    Create K Sorted Array.cpp

---
+++ Heap/K Sorted Array.cpp
@@ -0,0 +1,29 @@
+Problem Description : https://practice.geeksforgeeks.org/problems/k-sorted-array1610/1
+Given an array of n distinct elements. Check whether the given array is a k sorted array or not. A k sorted array is an array where each element is at most k distance away from its target position in the sorted array.
+  class Solution {
+  public:
+    string isKSortedArray(int arr[], int n, int k)
+    {
+        //code here.
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>minh;
+        for(int i=0;i<n;i++)
+        {
+            minh.push({arr[i],i});
+        }
+        int i = 0;
+         while(minh.size()!=0)
+         {
+
+               if(abs(q.top().second-i) <= k){
+                   minh.pop();
+                   i++;
+               }
+               else{
+                   return "No";
+               }
+        
+        
+         }
+         return "Yes";
+    }
+  };
